26321f386e9fe4a3582a6b3194e70609961fd5c1
stan <stan@stan.com>
2020-06-06 09:49:47 +0800

    haha

--- Scripts/test_001.py
+++ Scripts/test_001.py
@@ -8,16 +8,15 @@ class Test001:
     @allure.step("登录")
     def test_002(self):
         allure.attach("用户名：popo,密码：123456", "附件")  # 添加步骤描述信息
-        assert False
+
 
 
     @allure.step("支付")
     def test_003(self):
         allure.attach("买一个手机", "附件")  # 添加步骤描述信息
-        assert False
+
 
     @allure.step("查询")
     def test_004(self):
         allure.attach("买一个手机", "附件")  # 添加步骤描述信息
         print("hahaa")
-        assert False
